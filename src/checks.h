b4c3d88e9643748547d7affc5dec16abdb4d1cbb
Cheng Luyu <chengluyu@live.cn>
2016-06-20 19:54:32 +0800

    Add checking utilities.

---
+++ src/checks.h
@@ -0,0 +1,15 @@
+#ifndef FLORA_CHECKS_H
+#define FLORA_CHECKS_H
+
+#include <cstdlib>
+#include <cstdio>
+
+[[noreturn]] static void simpjs_fatal(const char *file, int line, const char *message) {
+  std::printf("FATAL ERROR at line %d in file '%s': %s", line, file, message);
+  std::abort();
+}
+
+#define UNREACHABLE() simpjs_fatal(__FILE__, __LINE__, "unreachable code")
+#define UNIMPLEMENTED() simpjs_fatal(__FILE__, __LINE__, "unimplemented code")
+
+#endif
